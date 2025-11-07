cf890aacc5a3083f22527504803d2fff8f545a92
rishabh1435 <rishabhgupta4@outlook.com>
2025-11-07 16:18:10 -0500

    Add files via upload

---
+++ MileCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MileCalculator;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        string distance = "";
+        Console.WriteLine("Enter from zip code!");
+        string fromZip = Console.ReadLine();
+        Console.WriteLine("Enter destination code!");
+        string toZip = Console.ReadLine();
+        ZipDistance obj = new ZipDistance();
+        distance= await obj.GetDistance(fromZip, toZip);
+        Console.WriteLine("Distance -"+ distance);
+        Console.ReadLine();
+    }
+}
---
+++ MileCalculator/ZipDistance.cs
@@ -0,0 +1,36 @@
+﻿using Google.Apis;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+namespace MileCalculator
+{
+    public class ZipDistance
+    {
+
+   
+        public async Task<string> GetDistance(string fromzip, string tozip)
+        {
+           string ApiBaseUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&";
+         
+            string _apiKey = "AIzaSyCHWZXFKpqInWGRR6cq7fLKoEIq4Yk6RD0";
+
+            using (HttpClient client = new HttpClient())
+            {
+                string requestUrl = ApiBaseUrl+"origins="+fromzip+"&destinations="+tozip+"&key="+_apiKey+"";
+                HttpResponseMessage response = await client.GetAsync(requestUrl);
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                int index1 = jsonResponse.IndexOf("text");
+                int index2 = jsonResponse.IndexOf("mi");
+                string substringpart = jsonResponse.Substring(index1+7, index2-index1-4);
+                return substringpart;
+
+            }
+        }
+
+
+    }
+}
